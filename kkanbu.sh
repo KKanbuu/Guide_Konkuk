5714e35471f45eb2b65d4fedb1a2eeeb0cb94759
privateJh <kjhoon9674@gmail.com>
2022-04-16 01:34:18 +0900

    Update kkanbu.sh
    1. Open Terminal
    2. Go to git workspace
    3. "sh kkanbu.sh"

--- kkanbu.sh
+++ kkanbu.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-gnome-terminal -e  " roslaunch global_planning global_planner.launch " &
-sleep 1
-gnome-terminal  -e  " roslaunch kkanbu_tf broadcast_tf.launch "  &
-sleep 1
-gnome-terminal  -e  " roslaunch kkanbu_visualization visualization.launch "  &
-sleep 1
-gnome-terminal  -e  " roslaunch communication_arduino communication.launch "  &
-sleep 3
-gnome-terminal  -e  " roslaunch kkanbu_control control.launch " 
+source /home/jh/Documents/GitHub/Guide_Konkuk/devel/setup.bash
+
+roslaunch global_planning global_planner.launch &
+sleep 6
+roslaunch kkanbu_tf broadcast_tf.launch &
+roslaunch kkanbu_visualization visualization.launch &
+roslaunch communication_arduino communication.launch &
+roslaunch kkanbu_control control.launch 
+# gnome-terminal -e  " roslaunch global_planning global_planner.launch " &
+# sleep 1
+# gnome-terminal  -e  " roslaunch kkanbu_tf broadcast_tf.launch "  &
+# sleep 1
+# gnome-terminal  -e  " roslaunch kkanbu_visualization visualization.launch "  &
+# sleep 1
+# gnome-terminal  -e  " roslaunch communication_arduino communication.launch "  &
+# sleep 3
+# gnome-terminal  -e  " roslaunch kkanbu_control control.launch " 
 # gnome-terminal -e  " roslaunch vehicle_control vehicle_control.launch "
